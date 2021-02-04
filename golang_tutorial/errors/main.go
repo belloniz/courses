ad6849f537079dc98b84df5b10488cb300f47b97
belloniz <gabriel.belloni@outlook.com>
2021-02-04 19:37:24 -0300

    New files

---
+++ golang_tutorial/errors/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main()  {
+	f, err := os.Open("/test.txt")
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+	//fmt.Println(f.Name(), "opened sucessfully")
+	//fmt.Println()
+
+	if err != nil {
+		if pErr, ok := err.(*os.PathError); ok {
+			fmt.Println("Failed to open file at path", pErr.Path)
+			return
+		}
+		fmt.Println("Generic error", err)
+		return
+	}
+	fmt.Println(f.Name(), "opened sucessfully")
+}
